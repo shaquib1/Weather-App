d9f7279c24b6c556dba89bd853fabea14c6b88a7
Md Shaquib Alam <shaquib475@gmail.com>
2024-02-03 17:52:17 +0530

    merge github

--- src/App.js
+++ src/App.js
@@ -192,7 +192,7 @@ function App() {
 
             <UTCDatetime />
             <Link
-              href="https://github.com/Amin-Awinti"
+              href="https://github.com/shaquib1"
               target="_blank"
               underline="none"
               sx={{ display: 'flex' }}
